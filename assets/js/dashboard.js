23ee4a764241cc52fb87f08dfa8a8aa00076db1a
Rayraylu <57138691+Rayraylu@users.noreply.github.com>
2021-05-24 17:10:40 +0800

    Update dashboard.js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,3 +1,10 @@
-function myFunction() {
+/*
+function dbFunction() {
     document.querySelector("launcher-bar-desktop > div > button");
+}
+*/
+function dbFunction() {
+    document.getElementById("launcher-bar-desktop").classList.toggle("show");
+    launcher - bar - desktop.classList.toggle("show"); //現在是增加
+    launcher - bar - desktop.classlist.toggle("show"); //現在是刪除
 }
