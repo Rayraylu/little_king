0efd168e419d5c290732bb3579beebaf702b9aa4
Rayraylu <57138691+Rayraylu@users.noreply.github.com>
2021-06-14 19:29:29 +0800

    Update dashboard_test.js

--- assets/js/dashboard_test.js
+++ assets/js/dashboard_test.js
@@ -3,6 +3,21 @@ function dbFunction() {
     document.querySelector("launcher-bar-desktop > div > button");
 }
 */
+var dbcontent;
+window.onload = function() {
+    dbcontent = document.getElementById("dbcontent");
+};
+
+function dashboard() {
+    if (dbcontent.style.display === "none") {
+        dbcontent.style.display = "block";
+    } else {
+        dbcontent.style.display = "none";
+    }
+}
+
+
+/*
 function dashboard() {
     document.getElementById("myDB").classList.toggle("show");
     myDB.classList.toggle("show"); //現在是增加
@@ -12,3 +27,4 @@ function dashboard() {
 document.onclick = function() {
     document.getElementsById("myDB").style.display = "none";
 }
+*/
